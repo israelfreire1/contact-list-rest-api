29aa3cf2d5cae0ba3aff7f4dca7e73cea8e408f2
israel-freire14 <israelfreire999@gmail.com>
2024-07-02 10:15:20 -0300

    feat: Adding log.info() in endpoints at API

--- src/main/kotlin/com/israel/teste/controllers/ContactController.kt
+++ src/main/kotlin/com/israel/teste/controllers/ContactController.kt
@@ -3,8 +3,8 @@ package com.israel.teste.controllers
 import com.israel.teste.entities.Contact
 import com.israel.teste.repositories.ContactRepository
 import com.israel.teste.services.ContactService
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.apache.logging.log4j.LogManager
-import org.slf4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,11 +14,12 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import javax.persistence.EntityNotFoundException
 
-@Suppress("UNREACHABLE_CODE")
+
+
 @RestController // create a controller
 @RequestMapping ("/contacts")//giving a request name for a resource
+@Tag(name = "Contact list", description = "Api to store contacts")
 class ContactController {
 
     private val logger = LogManager.getLogger(ContactController::class.java)
@@ -39,10 +40,8 @@ class ContactController {
     //Do a POST type request at API endpoint
     @PostMapping
     fun create(@RequestBody contact: Contact ): Contact {
+        logger.info("Waiting for POST request to /contacts with a CONTACT ins JSON body. Will return status 200 with ${contact} $this")
         return service.addContact(contact)
-
-        logger.info("m=Contact added by ContactService ${contact} $this")
-
     }
 
     //Do a GET type request at API endpoint
@@ -50,18 +49,21 @@ class ContactController {
     //Getting query parameter through @PathVariable and body JSON through @RequestBody
     fun show(@PathVariable("id") id : Long): Contact {
         //return repository.findById(id).orElseThrow {EntityNotFoundException()}
+        logger.info("Waiting for GET request to /contacts/${id} with ID in JSON body. Will Return status 200 with user ID in response.${this}")
         return service.showContact(id)
     }
 
     //Do a PUT type request at API endpoint
     @PutMapping("/{id}")
     fun update(@PathVariable("id") id: Long, @RequestBody newContact: Contact) : Contact{
+        logger.info("Waiting for PUT request to /contacts/${id} with a new name, email e phone number in JSON body. Will return status 200 with contact saved in repository. ${this}")
        return service.alterContact(id, newContact)
     }
 
     //Do a DELETE type request at API endpoint
     @DeleteMapping("/{id}")
     fun delete(@PathVariable("id") id: Long) {
-       service.deleteContact(id)
+        logger.info("Waiting for DELETE request to /contacts/${id} with valid contact ID in URL. Will return status 200 with no response body. ${this}")
+        service.deleteContact(id)
     }
 }
